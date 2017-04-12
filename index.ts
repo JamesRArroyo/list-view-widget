8dabf732418bd32204518eefe4e0ba0f21827bec
James Arroyo <James Arroyo>
2017-04-12 12:08:45 -0600

    added module to manage component and exported module instead of component

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 
-export {ListViewWidgetComponent} from './src/list-view-widget.component';
+export {ListViewWidgetModule} from './src/list-view-widget.module';
---
+++ src/list-view-widget.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {ListViewWidgetComponent} from './list-view-widget.component';
+
+
+@NgModule({
+    declarations: [
+        ListViewWidgetComponent
+    ],
+    imports: [
+    // Add pipes here after they have their own npm packages
+    ],
+    providers: [
+
+    ],
+    exports: [
+        ListViewWidgetComponent
+    ]
+})
+
+export class ListViewWidgetModule {}
